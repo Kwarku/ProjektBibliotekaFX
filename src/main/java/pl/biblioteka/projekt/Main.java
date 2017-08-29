1559ca39e9c860b017fcba1559301eb074a414af
Kwarku <p.kuklicz@gmail.com>
2017-08-29 14:58:00 +0200

    stowrzenie Internacjonalizacji i Resource bundles

--- src/main/java/pl/biblioteka/projekt/Main.java
+++ src/main/java/pl/biblioteka/projekt/Main.java
@@ -1,10 +1,48 @@
 package pl.biblioteka.projekt;
 
+import com.sun.istack.internal.localization.Localizable;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.BorderPane;
 import javafx.stage.Stage;
+
+import java.util.Locale;
+import java.util.ResourceBundle;
+
+/*      INTERNACJONALIZACJA
+* Aby wprowadzic mozliwosc zmiany jezyka w calej aplikacji nalezy:
+* - upewnic sie ze mamy poprawny sposob kodowania w srodowisku ( w intellij to Editor -> file encodings UTF-8 wszedzie)
+* - w folderze resources stworzyc nowy folder o nazwie bundles
+* - stworzyc nowy i wybrac Resource Bundle
+* - na gorze wybieramy nazwe, i po prawej dodajemy lokalizajce pl (jezeli chcemy wiecej jezykow luz)
+* - powstaja pliki do kazdego jezyka i default, gdy system nie wykryje wlasciwej lokalizacji uzyje default
+* - wchodzimy do pliku, na dole wybieramy z zakladki Resource Bundle i:
+* - dodajemy klucz nastepnie dodajemy tresc czyli napis jaki ma byc: np title.app -> default Application, pl Aplikacja
+*
+* - podpinamy w klasie main nasze bundle
+* - uzywamy klasy ResourceBundles i metody statycznej getBundle o tak:
+*   ResourceBundle XXXXXXX = ResourceBundle.getBundle("bundles.messages")
+* - podajemy sciezke tylko do glownej nazwy tych bundlesow
+*
+* - nastepnie uzywamy loadera zeby dodac resoursy
+*   loader.setResources(resourceBundle);
+*
+* - teraz gdy chcemy uzyc tego tytulu w pliku javy nalezy uzyc metody XXXXX.getString
+*  oraz podac klucz tego stringa np "title.application" no i dziala
+*
+* ABY podac to w Scene Builderze nalezy :
+* - w zakladce Preview - > Internaationalien -> set Resources i podac nasz wybrany plik jezykowy
+* - wejsc w dowolny np przycisk z napisem
+* - wybrac rubryke Text
+* - z opcji wybrac Replace with Internacionalize String
+* - podac interesujacy nas klucz
+*
+* - aby sprawdzic czy dziala nalezy zmienic lokalizacje:
+* sluzy do tego metoda Locale.setDefault(new Locale("en" albo inny jezyk ))
+*
+* */
+
 
 public class Main extends Application{
     public static void main(String[] args) {
@@ -13,12 +51,16 @@ public class Main extends Application{
 
 
     public void start(Stage primaryStage) throws Exception {
-
+//        Locale.setDefault(new Locale("en"));
         FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/BorderPaneMain.fxml"));
+
+        ResourceBundle bundle = ResourceBundle.getBundle("bundles.messages");
+        loader.setResources(bundle);
+
         BorderPane borderPane = loader.load();
         Scene scene = new Scene(borderPane);
         primaryStage.setScene(scene);
-        primaryStage.setTitle("Biblioteka");
+        primaryStage.setTitle(bundle.getString("title.application"));
         primaryStage.show();
 
 
